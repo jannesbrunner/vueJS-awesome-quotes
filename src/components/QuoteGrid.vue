9e162b58802bbbc184c12fbe5bbb2b70d1783b9e
Jannes Brunner <jannes.brunner@acticom.de>
2020-08-11 11:00:12 +0200

    Display example quote iterrative

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="row">
-        
+        <app-quote v-for="quote in quotes" v-bind:key="quote">
+            <p>{{ quote }}</p>
+        </app-quote>
     </div>
 </template>
 
 <script>
+import Quote from './Quote.vue';
 export default {
-    props: ['quotes']
+    props: ['quotes'],
+    components: {
+        appQuote: Quote
+    }
 }
 </script>
 
