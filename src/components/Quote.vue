6498a9ae64a1b19413b814f8d7f13513bce5c2c8
Jannes Brunner <jannes.brunner@acticom.de>
2020-08-11 10:46:47 +0200

    create quotes component

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,11 +1,14 @@
 <template>
-    
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    
-}
 </script>
 
 <style scoped>
@@ -13,5 +16,13 @@ export default {
         font-family: 'Arizonia', cursive;
         font-size: 24px;
         color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
     }
 </style>
