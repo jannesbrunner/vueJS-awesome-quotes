5f9c4bbc74c1dbd73fcddbbbf3a210fb78a186c1
Jannes Brunner <jannes.brunner@acticom.de>
2020-08-11 11:08:49 +0200

    Add + implement 'new Quote' component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="row">
+       <div class="cols-sm8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+           <label>Quote<label>
+            <textarea class="form-control" rows="3" v-model="quote"></textarea>
+       </div>
+       <div class="cols-sm8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+           <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            quote: ''
+        };
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteNew', this.quote);
+            this.quote = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
